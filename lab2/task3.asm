4c2c25aff606d0a6e1a7ca51d982bbfa1eb7f62d
Marina <nozdryakovamarina94@gmail.com>
2024-10-09 22:00:33 +0400

    task3  in the lab2

---
+++ lab2/task3.asm
@@ -0,0 +1,24 @@
+; log_2 (tg(x+a))=b
+; x = arctg(2^b)-a
+; 2 b ^ arctg a -
+section .rodata
+    a:  dd 5.0
+    b:  dd 3.0 
+
+section .bss
+    x: resd 1
+
+section .text 
+global main
+
+main: 
+    fld dword [b]
+    fld1 
+    fscale
+    fstp st1
+    fld1 
+    fpatan
+    fld dword [a]
+    fsub
+    fstp dword [x]
+    ret
